d862fadc4778a8514161a2f994b335c83a653766
AparisiuDev <aparisiu.evolution@gmail.com>
2025-09-23 11:57:50 +0200

    Creada la clase Equipo con solo las propiedades y atributos

---
+++ App Futbol/Equipo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Futbol
+{
+    internal class Equipo
+    {
+        private string _nombre;
+        private string _nacionalidad;
+        private int _puntos;
+        public Equipo()
+        {
+
+        }
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+        public string Nacionalidad
+        {
+            get { return _nacionalidad; }
+            set { _nombre = value; }
+        }
+        public int Puntos
+        {
+            get { return _puntos; }
+            set { _puntos = value; }
+        }
+    }
+}
