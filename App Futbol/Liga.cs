e67171f50d0e822a56f6814020c9605ece4c2ac3
AparisiuDev <aparisiu.evolution@gmail.com>
2025-09-25 10:57:12 +0200

    Añadido la clase Liga

---
+++ App Futbol/Liga.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Futbol
+{
+    internal class Liga
+    {
+        private List<Equipo> _laLiga;
+
+        public List<Equipo> LaLiga
+        {
+            get { return _laLiga; }
+            set { _laLiga = value; }
+        }
+
+        public void OrdenarPorPuntos()
+        {
+            // Ordenar de mayor a menor
+            _laLiga = _laLiga.OrderByDescending(e => e.Puntos).ToList();
+        }
+
+        public void MostrarClasificacion()
+        {
+            OrdenarPorPuntos();
+
+            Console.WriteLine("=== Clasificación de LaLiga ===");
+            int pos = 1;
+            foreach (var equipo in _laLiga)
+            {
+                Console.WriteLine($"{pos}. {equipo.Nombre} - {equipo.Puntos} pts");
+                pos++;
+            }
+        }
+
+    }
+}
